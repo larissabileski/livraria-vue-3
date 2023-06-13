46206aa7b4f06f91eecb3a9fac05899d3e12acf4
larissabileski <laricristinabileski@gmail.com>
2023-06-13 14:59:10 -0300

    App atualizada

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <header>
-    <router-link :to="{ name: 'home' }">Home</router-link>
-    <router-link to="/livros">Livros</router-link>
-    <router-link to="/categorias">Categorias</router-link>
-    <router-link to="/editoras">Editoras</router-link>
-    <router-link to="/autores">Autores</router-link>
+    <router-link :to="{ name: 'home' }">Home </router-link>
+    <router-link to="/livros">Livros </router-link>
+    <router-link to="/categorias">Categorias </router-link>
+    <router-link to="/editoras">Editoras </router-link>
+    <router-link to="/autores">Autores </router-link>
   </header>
   <main>
     <RouterView />
